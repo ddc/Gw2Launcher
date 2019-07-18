b6facb4479c13912a6480c6a396b27fa70b12500
ddc <ddc@users.noreply.github.com>
2019-07-18 19:13:27 -0300

    first commit

---
+++ src/utils/constants.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python3
+#|*****************************************************
+# * Copyright         : Copyright (C) 2019
+# * Author            : ddc
+# * License           : GPL v3
+# * Python            : 3.6
+#|*****************************************************
+# # -*- coding: utf-8 -*-
+import os
+import sys
+import platform
+import logging
+from src.utils import utils
+################################################################################
+################################################################################
+################################################################################
+PROGRAM_NAME            = "Guild Wars 2 Utilities"
+VERSION                 = "1.15"
+################################################################################
+exit_timer              = 5
+################################################################################
+################################################################################
+################################################################################
+############### DO NOT CHANGE ANY OF THESE VARS BELLOW #########################
+################################################################################
+################################################################################
+################################################################################
+short_program_name      = "Gw2Utils"
+full_program_name       = f"{PROGRAM_NAME} v{VERSION}"
+exe_program_name        = f"{short_program_name}.exe"
+################################################################################
+date_formatter          = "%b/%d/%Y"
+time_formatter          = "%H:%M:%S"
+LOG_LEVEL               = logging.INFO
+LOG_FORMATTER           = logging.Formatter('%(asctime)s:[%(levelname)s]:[%(filename)s:%(funcName)s:%(lineno)d]:%(message)s',
+                                datefmt=f"[{date_formatter} {time_formatter}]")
+################################################################################
+IS_WINDOWS              = os.name == "nt"
+IS_MAC                  = sys.platform == "darwin"
+IS_LINUX                = sys.platform == "linux"
+IS_64BIT                = platform.machine().endswith("64")
+PYTHON_OK               = sys.version_info >= (3,6)
+################################################################################
+gw2_64_bit_exec_name    = ["Gw2-64.exe"]
+################################################################################
+my_docs_path            = str(utils.get_my_documents_path())
+program_path            = f"{my_docs_path}/{short_program_name}"
+#data_path               = f"{program_path}/data"
+#logs_path               = f"{program_path}/logs"
+################################################################################
+settings_filename       = f"{program_path}/settings.ini"
+style_qss_filename      = f"{program_path}/style.qss"
+error_logs_filename     = f"{program_path}/errors.log"
+################################################################################
+arcdps_url              = "http://www.deltaconnected.com/arcdps"
+d3d9_url                = f"{arcdps_url}/x64/d3d9.dll"
+md5sum_url              = f"{arcdps_url}/x64/d3d9.dll.md5sum"
+buildTemplate_url       = f"{arcdps_url}/x64/buildtemplates/d3d9_arcdps_buildtemplates.dll"
+extras_url              = f"{arcdps_url}/x64/extras/d3d9_arcdps_extras.dll"
+github_exe_program_url  = f"https://github.com/ddc/{short_program_name}/releases/download/v{VERSION}/{exe_program_name}"
+remote_version_filename = f"https://raw.github.com/ddc/{short_program_name}/master/VERSION"
+################################################################################
+d3d9_path               = "/bin64/d3d9.dll"
+d3d9_bak_path           = "/bin64/d3d9.dll.bak"
+template_path           = "/bin64/d3d9_arcdps_buildtemplates.dll"
+template_bak_path       = "/bin64/d3d9_arcdps_buildtemplates.dll.bak"
+extras_path             = "/bin64/d3d9_arcdps_extras.dll"
+extras_bak_path         = "/bin64/d3d9_arcdps_extras.dll.bak"     
+################################################################################
---
+++ src/utils/messages.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python3
+#|*****************************************************
+# * Copyright         : Copyright (C) 2019
+# * Author            : ddc
+# * License           : GPL v3
+# * Python            : 3.6
+#|*****************************************************
+# # -*- coding: utf-8 -*-
+
+find_gw2_exec                   = "Please find \"Gw2-64.exe\" file before continue!!!"
+
+not_valid_gw2                   = "is not a valid Gw2 executable!!!\nThis program needs a valid GW2 file."
+
+dl_new_version                  = "Downloading new version..."
+
+dl_new_version_timeout          = "Unable to reach Website to check for new version!!!"
+
+error_dl_new_version            = "There was an ERROR trying to download new version.\nPlease try again later."
+
+error_check_new_version         = "There was an ERROR trying to check for a new program version!"
+
+info_dl_completed               = "Download completed successfully.\nNew version downloaded to \"Downloads\" directory."
+
+checking_new_version            = "Checking for new program version: "
+
+new_version_available           = "There's a new version available!!!"
+
+check_downloaded_dir            = "If you already downloaded the new version,\nplease check your download directory!!!"
+
+confirm_download                = "Do you want to download the new version now?"
+
+remote_version_file_not_found   = "ERROR: Remote File: VERSION was not found."
+
+need_find_gw2                   = "<== NEED TO FIND \"Gw2-64.exe\" TO ENABLE ALL PROGRAM FUNCTIONALITIES!!!"
+
+gw2_32bit_not_supported         = "32bits Guild Wars 2 is not Supported!!!"
+
+arcdps_404                      = "Could not find arcdps file to download from the website!!!!"
+
+arcdps_unreacheable             = "Unable to reach Arcdps website!!!"
+
+arcdps_timeout                  = "Unable to reach Arcdps website to check for updates!!!"
+
+arcdps_new_version              = "Checking for new version of ArcDps: "
+
+arcdps_updating                 = "Updating ArcDps: "
+
+arcdps_installing               = "Installing ArcDps: "
+
+arcdps_error_dl                 = "Error downloading arcdps!!!"
+
+arcdps_error_install            = "Error installing arcdps!!!"
+
+arcdps_disclamer                = "I did not write this loader nor do i provide any support for it or it's compatibility with Guild Wars 2. Credit goes to /u/deltaconnected."
+
+arcdps_installed                = "Arcdps successfully installed !!!"
+
+arcdps_removed                  = "Arcdps successfully removed !!!"
